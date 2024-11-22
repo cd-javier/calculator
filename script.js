c52a813ea72cccb9bfe1014310f14a978475b57b
Javier Quiroga <contact@javierquiroga.com>
2024-11-22 19:55:24 +0000

    Add function to truncate result of operations

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
+// --------------------------
+// Math Functions
+// --------------------------
+
+// Basic Operations
 function add(a, b) {
   return a + b;
 }
-
 function subtract(a, b) {
   return a - b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   return a / b;
 }
 
-let previousNumber;
-let operator;
-let currentNumber;
-
+// Operator
 function operate(operator, previousNumber, currentNumber) {
   let result;
 
   switch (operator) {
-    case add:
+    case 'add':
       result = add(previousNumber, currentNumber);
       break;
-    case subtract:
+    case 'subtract':
       result = subtract(previousNumber, currentNumber);
       break;
-    case multiply:
+    case 'multiply':
       result = multiply(previousNumber, currentNumber);
       break;
-    case divide:
+    case 'divide':
       result = divide(previousNumber, currentNumber);
       break;
   }
 
-  return result;
+  return truncate(result);
 }
+
+// Function to truncate the result to fit on display
+function truncate(number) {
+  const intLength = Math.floor(number).toString().length;
+  const numOfTrunc = 10 ** (11 - intLength);
+  const truncated = Math.floor(number * numOfTrunc) / numOfTrunc;
+  return truncated
+}
+
+// --------------------------
+// Basic variables
+// --------------------------
+let previousNumber;
+let operator;
+let currentNumber;
